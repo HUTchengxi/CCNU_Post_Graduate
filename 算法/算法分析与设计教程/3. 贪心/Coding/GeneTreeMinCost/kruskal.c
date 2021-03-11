7ce0b0b264ae8ddd2955e810dd2390e7907aeb49
Jream.Y <yinjimin@iboxpay.com>
2021-03-12 00:23:04 +0800

    贪心算法：最小生成树 & Kruskal算法，明天优化

---
+++ 算法/算法分析与设计教程/3. 贪心/Coding/GeneTreeMinCost/kruskal.c
@@ -0,0 +1,259 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <malloc.h>
+#include <string.h>
+
+#define MAX         100                 // 矩阵最大容量
+#define INF         (~(0x1<<31))        // 最大值(即0X7FFFFFFF)
+#define isLetter(a) ((((a)>='a')&&((a)<='z')) || (((a)>='A')&&((a)<='Z')))
+#define LENGTH(a)   (sizeof(a)/sizeof(a[0]))
+
+// 邻接矩阵
+typedef struct _graph
+{
+    char vexs[MAX];       // 顶点集合
+    int vexnum;           // 顶点数
+    int edgnum;           // 边数
+    int matrix[MAX][MAX]; // 邻接矩阵
+}Graph, *PGraph;
+
+// 边的结构体
+typedef struct _EdgeData
+{
+    char start; // 边的起点
+    char end;   // 边的终点
+    int weight; // 边的权重
+}EData;
+
+
+/*
+ * 返回ch在matrix矩阵中的位置
+ */
+static int get_position(Graph G, char ch)
+{
+    int i;
+    for(i=0; i<G.vexnum; i++)
+        if(G.vexs[i]==ch)
+            return i;
+    return -1;
+}
+
+/*
+ * 读取一个输入字符
+ */
+static char read_char()
+{
+    char ch;
+
+    do {
+        ch = getchar();
+    } while(!isLetter(ch));
+
+    return ch;
+}
+
+/*
+ * 创建图(用已提供的矩阵)
+ */
+Graph* create_example_graph()
+{
+    char vexs[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G'};
+    int matrix[][9] = {
+             /*A*//*B*//*C*//*D*//*E*//*F*//*G*/
+      /*A*/ {   0,  12, INF, INF, INF,  16,  14},
+      /*B*/ {  12,   0,  10, INF, INF,   7, INF},
+      /*C*/ { INF,  10,   0,   3,   5,   6, INF},
+      /*D*/ { INF, INF,   3,   0,   4, INF, INF},
+      /*E*/ { INF, INF,   5,   4,   0,   2,   8},
+      /*F*/ {  16,   7,   6, INF,   2,   0,   9},
+      /*G*/ {  14, INF, INF, INF,   8,   9,   0}};
+    int vlen = LENGTH(vexs);
+    int i, j;
+    Graph* pG;
+    
+    // 输入"顶点数"和"边数"
+    if ((pG=(Graph*)malloc(sizeof(Graph))) == NULL )
+        return NULL;
+    memset(pG, 0, sizeof(Graph));
+
+    // 初始化"顶点数"
+    pG->vexnum = vlen;
+    // 初始化"顶点"
+    for (i = 0; i < pG->vexnum; i++)
+        pG->vexs[i] = vexs[i];
+
+    // 初始化"边"
+    for (i = 0; i < pG->vexnum; i++)
+        for (j = 0; j < pG->vexnum; j++)
+            pG->matrix[i][j] = matrix[i][j];
+
+    // 统计边的数目
+    for (i = 0; i < pG->vexnum; i++)
+        for (j = 0; j < pG->vexnum; j++)
+            if (i!=j && pG->matrix[i][j]!=INF)
+                pG->edgnum++;
+    pG->edgnum /= 2;
+
+    return pG;
+}
+
+/*
+ * 返回顶点v的第一个邻接顶点的索引，失败则返回-1
+ */
+static int first_vertex(Graph G, int v)
+{
+    int i;
+
+    if (v<0 || v>(G.vexnum-1))
+        return -1;
+
+    for (i = 0; i < G.vexnum; i++)
+        if (G.matrix[v][i]!=0 && G.matrix[v][i]!=INF)
+            return i;
+
+    return -1;
+}
+
+/*
+ * 返回顶点v相对于w的下一个邻接顶点的索引，失败则返回-1
+ */
+static int next_vertix(Graph G, int v, int w)
+{
+    int i;
+
+    if (v<0 || v>(G.vexnum-1) || w<0 || w>(G.vexnum-1))
+        return -1;
+
+    for (i = w + 1; i < G.vexnum; i++)
+        if (G.matrix[v][i]!=0 && G.matrix[v][i]!=INF)
+            return i;
+
+    return -1;
+}
+
+/*
+ * 打印矩阵队列图
+ */
+void print_graph(Graph G)
+{
+    int i,j;
+
+    printf("Martix Graph:\n");
+    for (i = 0; i < G.vexnum; i++)
+    {
+        for (j = 0; j < G.vexnum; j++)
+            printf("%10d ", G.matrix[i][j]);
+        printf("\n");
+    }
+}
+
+/* 
+ * 获取图中的边
+ */
+EData* get_edges(Graph G)
+{
+    int i,j;
+    int index=0;
+    EData *edges;
+
+    edges = (EData*)malloc(G.edgnum*sizeof(EData));
+    for (i=0;i < G.vexnum;i++)
+    {
+        for (j=i+1;j < G.vexnum;j++)
+        {
+            if (G.matrix[i][j]!=INF)
+            {
+                edges[index].start  = G.vexs[i];
+                edges[index].end    = G.vexs[j];
+                edges[index].weight = G.matrix[i][j];
+                index++;
+            }
+        }
+    }
+
+    return edges;
+}
+
+/* 
+ * 对边按照权值大小进行排序(由小到大)
+ */
+void sorted_edges(EData* edges, int elen)
+{
+    int i,j;
+
+    for (i=0; i<elen; i++)
+    {
+        for (j=i+1; j<elen; j++)
+        {
+            if (edges[i].weight > edges[j].weight)
+            {
+                // 交换"第i条边"和"第j条边"
+                EData tmp = edges[i];
+                edges[i] = edges[j];
+                edges[j] = tmp;
+            }
+        }
+    }
+}
+
+/*
+ * 获取i的终点
+ */
+int get_end(int vends[], int i)
+{
+    while (vends[i] != 0)
+        i = vends[i];
+    return i;
+}
+
+/*
+ * 克鲁斯卡尔（Kruskal)最小生成树
+ */
+void kruskal(Graph G)
+{
+    int i,m,n,p1,p2;
+    int length;
+    int index = 0;          // rets数组的索引
+    int vends[MAX]={0};     // 用于保存"已有最小生成树"中每个顶点在该最小树中的终点。
+    EData rets[MAX];        // 结果数组，保存kruskal最小生成树的边
+    EData *edges;           // 图对应的所有边
+
+    // 获取"图中所有的边"
+    edges = get_edges(G);
+    // 将边按照"权"的大小进行排序(从小到大)
+    sorted_edges(edges, G.edgnum);
+
+    for (i=0; i<G.edgnum; i++)
+    {
+        p1 = get_position(G, edges[i].start);   // 获取第i条边的"起点"的序号
+        p2 = get_position(G, edges[i].end);     // 获取第i条边的"终点"的序号
+
+        m = get_end(vends, p1);                 // 获取p1在"已有的最小生成树"中的终点
+        n = get_end(vends, p2);                 // 获取p2在"已有的最小生成树"中的终点
+        // 如果m!=n，意味着"边i"与"已经添加到最小生成树中的顶点"没有形成环路
+        if (m != n)
+        {
+            vends[m] = n;                       // 设置m在"已有的最小生成树"中的终点为n
+            rets[index++] = edges[i];           // 保存结果
+        }
+    }
+    free(edges);
+
+    // 统计并打印"kruskal最小生成树"的信息
+    length = 0;
+    for (i = 0; i < index; i++)
+        length += rets[i].weight;
+    printf("Kruskal=%d: ", length);
+    for (i = 0; i < index; i++)
+        printf("(%c,%c) ", rets[i].start, rets[i].end);
+    printf("\n");
+}
+
+void main()
+{
+    Graph* pG;
+
+    pG = create_example_graph();
+
+    kruskal(*pG);             // kruskal算法生成最小生成树
+}
