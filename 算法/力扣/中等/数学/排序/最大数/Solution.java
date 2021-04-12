71f3f978ebd4619c349a63a51d7dac5395dee151
Jream.Y <yinjimin@iboxpay.com>
2021-04-12 17:10:36 +0800

    力扣：排序 -> 最大数

---
+++ 算法/力扣/中等/数学/排序/最大数/Solution.java
@@ -0,0 +1,42 @@
+import java.util.Arrays;
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+        Solution solution = new Solution();
+        int[] nums = {10, 2, 5, 53, 22};
+        System.out.println(solution.largestNumber(nums));
+    }
+
+    public String largestNumber(int[] nums) {
+        int n = nums.length;
+        // 转换成包装类型，以便传入 Comparator 对象（此处为 lambda 表达式）
+        Integer[] numsArr = new Integer[n];
+        for (int i = 0; i < n; i++) {
+            numsArr[i] = nums[i];
+        }
+
+        // 排序原理：ab < ba 则a在b后面
+        Arrays.sort(numsArr, (x, y) -> {
+            long sx = 10, sy = 10;
+            while (sx <= x) {
+                sx *= 10;
+            }
+            while (sy <= y) {
+                sy *= 10;
+            }
+            return (int) (-sy * x - y + sx * y + x);
+        });
+
+        if (numsArr[0] == 0) {
+            return "0";
+        }
+        StringBuilder ret = new StringBuilder();
+        for (int num : numsArr) {
+            ret.append(num);
+        }
+        return ret.toString();
+    }
+
+}
